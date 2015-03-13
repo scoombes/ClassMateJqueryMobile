c81a275139528b86f8a8f939cc071e8d0f9375c6
Kyle Zimmerman <zimmerkyle@gmail.com>
2015-03-13 18:57:04 -0400

    Added login page. Fixed issue with piecharts when eventfeed isn't the first page loaded.

---
+++ JS/global.js
@@ -0,0 +1,25 @@
+$(document).on("pagecontainershow", function(event, ui) {
+    var activePage = $.mobile.pageContainer.pagecontainer("getActivePage").prop('id');
+    if (activePage != "eventfeed") {
+        return;
+    }
+
+    Chart.defaults.global.showTooltips = false;
+
+    $(".pie").each(function() {
+        var context = $(this)[0].getContext('2d');
+
+        var pieData = [
+            {
+                value: Math.floor((Math.random() * 20) + 1),
+                color: "#46E62D",
+            },
+            {
+                value: Math.floor((Math.random() * 20) + 1),
+                color: "#FB3140",
+            }
+        ];
+
+        new Chart(context).Pie(pieData, { animation: false, segmentStrokeColor: "#303030", segmentStrokeWidth: 2 });
+    });
+})
