cfd246e81c9a3e9c533f1ae461a5cf7d9ecbcd95
KyleZimmerman <zimmerkyle@gmail.com>
2015-03-25 16:54:01 -0400

    Added database.js and started event.js, posting for use as a sample

---
+++ JS/database.js
@@ -0,0 +1,6 @@
+//Init DB
+var db = openDatabase("classmateDB", "1.0", 2 * 1024 * 1024);
+
+
+//Init Tables
+Event.initialize();
---
+++ JS/event.js
@@ -0,0 +1,14 @@
+var Event =
+{
+	initialize: function() {
+		db.transaction(function(transaction) {
+
+		}, errorHandler);
+	},
+	insert: function() {
+
+	},
+	remove: function() {
+
+	}
+}
