889bb84352e6d3f6e44b873c2868c0476e58c803
KyleZimmerman <zimmerkyle@gmail.com>
2015-03-25 17:29:52 -0400

    Flushed out event.js and database.js

--- JS/database.js
+++ JS/database.js
@@ -1,6 +1,18 @@
 //Init DB
-var db = openDatabase("classmateDB", "1.0", 2 * 1024 * 1024);
+var db = openDatabase("classmateDB", "1.0", "ClassMate DB", 2 * 1024 * 1024);
 
+function errorHandler(transaction, error) {
+	var msg = "DB ERROR: " + error.message;
+	console.log(msg)
+	alert(msg);
+}
 
-//Init Tables
-Event.initialize();
+function intializeDatabase() {
+	Event.initialize();
+}
+
+function nukeDatabase() {
+	Event.nuke();
+}
+
+intializeDatabase();
