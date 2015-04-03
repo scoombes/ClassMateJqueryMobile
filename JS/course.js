dac29aa247a5bb09a67435b8926d1f51b5cbf8cd
Sean Coombes <seancoombes@live.ca>
2015-04-02 22:44:04 -0400

    most of creat even is up and running

--- JS/course.js
+++ JS/course.js
@@ -4,11 +4,11 @@ var Course =
 		db.transaction(function(transaction) {
 			var sql = "CREATE TABLE IF NOT EXISTS course ("
 				+ "id INTEGER PRIMARY KEY,"
-				+ "course_code VARCHAR,"
-				+ "section VARCHAR,"
-				+ "name DATETIME,"
-				+ "semester_id INTEGER,"
-				+ "year INTEGER,"
+				+ "course_code VARCHAR NOT NULL,"
+				+ "section VARCHAR NOT NULL,"
+				+ "name VARCHAR NOT NULL,"
+				+ "semester_id INTEGER NOT NULL,"
+				+ "year INTEGER NOT NULL,"
 				+ "teacher_name VARCHAR,"
 				+ "creator_id INTEGER"
 				+ ")"; 
@@ -39,6 +39,23 @@ var Course =
 				+ "WHERE user_course.user_id = ?";
 			transaction.executeSql(sql, [User.getCurrent().id], successCallback, errorHandler);
 		}, errorHandler);
+	},
+	populateList: function(){
+		db.transaction(function(transaction){
+			transaction.executeSql("SELECT * FROM course ORDER BY course_code DESC",[],
+				function(transaction, resultSet){
+					//var row = resultSet.rows.item();
+					var options, row;
+					for (var i = resultSet.rows.length - 1; i >= 0; i--) 
+					{
+						row = resultSet.rows.item(i);
+							options += '<option value="' + row.id + '">'
+								+ row.course_code
+								+ '</option>';
+					}
+					$("#eventcourse").html(options);
+				}, errorHandler);
+		});
 	},
 	nuke: function() {
 		db.transaction(function(transaction) {
