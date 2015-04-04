5a2e8f91e34ec9f1b61ca4fa111678300b779b82
Sean Coombes <seancoombes@live.ca>
2015-04-04 16:49:04 -0400

    changing how event feed works

--- JS/global.js
+++ JS/global.js
@@ -171,14 +171,23 @@ function createEventElement(dbItem) {
     };
 
     var eventElement = $("<li>").addClass("eventfeed-item");
-    eventElement.attr("data-row-id", event.id);
-    eventElement.attr("course-info", dbItem["course_code"] + "-" + dbItem["section"]);
-    eventElement.attr("course-id", dbItem["course_id"]);
+    
+    var display = $("<a>");
+    display.attr("event-id", event.id);
+    display.attr("event_course_info", dbItem["course_code"] + "-" + dbItem["section"]);
+    display.attr("event_course_id", dbItem["course_id"]);
 
-    var display = $("<a>").prop("href", "event-details.html");
+    display.click(function()
+    {
+        $.mobile.changePage("event-details.html", { data: { "event_id": $(this).attr("event-id") }, reloadPage: true, changeHash: true });
+    });
+
     display.append($("<h3>").addClass("course-name").text(event.courseCode));
     display.append($("<h2>").addClass("assignment-name").text(event.name));
     display.append($("<h3>").addClass("due-date").text(getDate(event.dueDate)));
+
+
+
     display.append($("<div>").addClass("vote-bar").append($("<div>").addClass("upvote-bar"))
                                                 .append($("<div>").addClass("downvote-bar")));
 
@@ -187,6 +196,8 @@ function createEventElement(dbItem) {
 
     var upvotePercent = upvotes / (upvotes + downvotes) * 100;
     var downvotePercent = downvotes / (upvotes + downvotes) * 100;
+
+    //HEY GUYS
     
     // display.find('.upvote-bar').css('height', upvotePercent + '%');
     // display.find('.downvote-bar').css('height', downvotePercent + '%');
