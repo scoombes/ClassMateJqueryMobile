20023057b33534ecaed7566ff058206281d384ed
Kyle Zimmerman <zimmerkyle@gmail.com>
2015-04-12 15:53:33 -0400

    Cleaned up push code

--- JS/push.js
+++ JS/push.js
@@ -1,86 +1,30 @@
-// document.addEventListener('deviceready', function() {
-// 	var appID = "YstnFpcnRNYfA35BEVOF84uAScfrhfO7Qw05Y2pU";
-// 	var clientKey = "TyI6sYmPBzgWWT6UMvAucWhTcLevnfkUVhsGFjFW";
-// 	var eventkey = "myEventKey";
-
-
-// 	alert('registering');
-
-// 	parsePlugin.initialize(appID, clientKey, function() {
-// 		alert('success');
-// 	}, function(e) {
-// 		alert('error registering device: ' + e);
-// 	});
-// }, false);
-
-// function getInstallation() {
-// 	ParsePushPlugin.getInstallationId(function(id) {
-//         alert(id);
-//         subscribe();
-//     }, function(e) {
-//         alert('error');
-//     });
-// }
-
-// function subscribe() {
-// 	ParsePushPlugin.subscribe('SampleChannel', function(msg) {
-//         alert('OK');
-//         listSubscriptions();
-//     }, function(e) {
-//         alert('error');
-//     });
-// }
-
-// function listSubscriptions() {
-// 	ParsePushPlugin.getSubscriptions(function(subscriptions) {
-//         alert(subscriptions);
-//     }, function(e) {
-//         alert('error');
-//     });
-// }
-
 document.addEventListener('deviceready', function() {
 	var appID = "YstnFpcnRNYfA35BEVOF84uAScfrhfO7Qw05Y2pU";
 	var clientKey = "TyI6sYmPBzgWWT6UMvAucWhTcLevnfkUVhsGFjFW";
 	var eventkey = "myEventKey";
-
-
-	alert('registering');
 
 	ParsePushPlugin.register({
 		appId: appID,
 		clientKey: clientKey,
 		eventKey: eventkey
 	}, function() {
-		alert('success');
-		getInstallation();
+		Push.subscribe('all');
 	}, function(e) {
-		alert('error registering device: ' + e);
+		alert('Error Registering Device: ' + e);
 	});
 }, false);
 
-function getInstallation() {
-	ParsePushPlugin.getInstallationId(function(id) {
-        alert(id);
-        subscribe();
-    }, function(e) {
-        alert('error');
-    });
-}
-
-function subscribe() {
-	ParsePushPlugin.subscribe('SampleChannel', function(msg) {
-        alert('OK');
-        listSubscriptions();
-    }, function(e) {
-        alert('error');
-    });
-}
-
-function listSubscriptions() {
-	ParsePushPlugin.getSubscriptions(function(subscriptions) {
-        alert(subscriptions);
-    }, function(e) {
-        alert('error');
-    });
-}
+var Push = {
+	subscribe: function(channelName, success, error) {
+		ParsePushPlugin.subscribe(channelName, success, error);
+	},
+	unsubscribe: function(channelName, success, error) {
+		ParsePushPlugin.unsubscribe(channelName, success, error);
+	},
+	send: function(data, channel) {
+		return Parse.Push.send({
+			chennels: [channel],
+			data: message
+		});
+	}
+};
