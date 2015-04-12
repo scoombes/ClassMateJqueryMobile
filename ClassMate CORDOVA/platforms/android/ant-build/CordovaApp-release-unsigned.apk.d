caaa0077720fde8579bc986ec03154a1c1f75c70
Kyle Zimmerman <zimmerkyle@gmail.com>
2015-04-12 17:41:56 -0400

    Added missing files

---
+++ ClassMate CORDOVA/platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,6 @@
+C:\Users\Kyle\Desktop\School\Year 2\Semester 2\Mobile Prog\classmate\ClassMate CORDOVA\platforms\android\ant-build\CordovaApp-release-unsigned.apk : \
+C:\Users\Kyle\Desktop\School\Year 2\Semester 2\Mobile Prog\classmate\ClassMate CORDOVA\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Users\Kyle\Desktop\School\Year 2\Semester 2\Mobile Prog\classmate\ClassMate CORDOVA\platforms\android\ant-build\classes.dex \
+C:\Users\Kyle\Desktop\School\Year 2\Semester 2\Mobile Prog\classmate\ClassMate CORDOVA\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\Users\Kyle\Desktop\School\Year 2\Semester 2\Mobile Prog\classmate\ClassMate CORDOVA\platforms\android\libs\Parse-1.8.1.jar \
+C:\Users\Kyle\Desktop\School\Year 2\Semester 2\Mobile Prog\classmate\ClassMate CORDOVA\platforms\android\libs\bolts-android-1.1.4.jar \
