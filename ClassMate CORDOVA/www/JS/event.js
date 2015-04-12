18c7ebe2ff5e171342cd15e2f85e40cfce7599c4
Kyle Zimmerman <zimmerkyle@gmail.com>
2015-04-12 14:26:15 -0400

    Built apk, including parse

--- ClassMate CORDOVA/www/JS/event.js
+++ ClassMate CORDOVA/www/JS/event.js
@@ -3,90 +3,102 @@
  *
  * 		Kyle Zimmerman - 3/25/15 js file created
  */
+var EventObject = Parse.Object.extend("Event");
 var Event =
 {
-	//Creates the table if required
-	initialize: function() {
-		db.transaction(function(transaction) {
-			var sql = "CREATE TABLE IF NOT EXISTS event ("
-				+ "id INTEGER PRIMARY KEY,"
-				+ "course_id INTEGER NOT NULL,"
-				+ "event_type INTEGER NOT NULL,"
-				+ "name VARCHAR NOT NULL,"
-				+ "due_date DATE NOT NULL,"
-				+ "time VARCHAR,"
-				+ "final_grade_weight INTEGER,"
-				+ "description VARCHAR,"
-				+ "creator_id INTEGER"
-				+ ")"; 
-
-			transaction.executeSql(sql, [], null, errorHandler);
-
-		}, errorHandler);
-	},
 	//Inserts a course into the database. Note: Validation should be done before calling this function
-	insert: function(course_id, event_type, name, due_date, time, final_grade_weight, description, creator_id) {
-		db.transaction(function(transaction) {
-			var sql = "INSERT INTO event ("
-				+ "course_id,"
-				+ "event_type,"
-				+ "name,"
-				+ "due_date,"
-				+ "time,"
-				+ "final_grade_weight,"
-				+ "description,"
-				+ "creator_id"
-				+ ") VALUES (?,?,?,?,?,?,?,?)";
+	insert: function(course_id, event_type, name, due_date, time, final_grade_weight, description) {
+		var event = new EventObject();
 
-			transaction.executeSql(sql, [course_id, event_type, name, due_date, time, final_grade_weight, description, creator_id],
-			 function (transaction, resultSet){
-				$.mobile.changePage("event-feed.html", {transition: "none"});
-			 }, errorHandler);
-		}, errorHandler);
+		event.set("course", CourseObject.createWithoutData(course_id));
+		event.set("eventType", EventTypeObject.createWithoutData(event_type));
+		event.set("name", name);
+		if (time == "") {
+			time = "00:00";
+		}
+		var dueDate = new Date(due_date + " " + time + ":00");
+		event.set("dueDate", dueDate);
+		event.set("finalGradeWeight", parseInt(final_grade_weight));
+		event.set("description", description);
+		event.set("creator", User.getCurrent());
+		event.save().then(function () {
+			$.mobile.changePage("event-feed.html", {transition: "none"});
+		}, parseErrorHandler);
 	}, 
 	//Gets a specific event by ID
-	read: function (id, successCallBack) {
-		db.transaction(function (transaction) {
-			var sql = "SELECT *, upvotes.count AS upvotes, downvotes.count as downvotes FROM event "
-					+ "LEFT OUTER JOIN (SELECT COUNT(value) AS count, vote.event_id AS event_id2 FROM vote WHERE value > 0 GROUP BY event_id2) AS upvotes ON upvotes.event_id2 = ? "
-					+ "LEFT OUTER JOIN (SELECT COUNT(value) AS count, vote.event_id AS event_id2 FROM vote WHERE value < 0 GROUP BY event_id2) AS downvotes ON downvotes.event_id2 = ? "
-					+ " WHERE id = ?";
-					
-			transaction.executeSql(sql, [id, id, id], successCallBack, errorHandler);
+	read: function (id, successCallback) {
+		var query = new Parse.Query(EventObject);
+		query.get(id).then( function (event) {
+			successCallback(event);
 		});
 	},
 	//Gets all of the events for a specific course
-	getEventsForCourse: function(courseId, successCallback) {
-		db.transaction(function(transaction) {
-			var sql = "SELECT *, event.id AS event_id, event.name AS name, upvotes.count AS upvotes, downvotes.count AS downvotes FROM event "
-					+ "JOIN course ON event.course_id = course.id "
-					+ "LEFT OUTER JOIN (SELECT COUNT(value) AS count, vote.event_id AS event_id2 FROM vote WHERE value > 0 GROUP BY event_id2) AS upvotes ON upvotes.event_id2 = event_id "
-					+ "LEFT OUTER JOIN (SELECT COUNT(value) AS count, vote.event_id AS event_id2 FROM vote WHERE value < 0 GROUP BY event_id2) AS downvotes ON downvotes.event_id2 = event_id "
-					+ "WHERE course_id = ? "
-					+ "ORDER BY due_date ASC";
+	getEventsForCourse: function(course_id, successCallback) {
+		var query = new Parse.Query(EventObject);
+		var eventsForCourse = [];
 
-			transaction.executeSql(sql, [courseId], successCallback, errorHandler);
-		});
+		Course.getCoursePromise(course_id).then(function (course) {
+			query.equalTo("course", course);
+			query.include('course');
+			query.include('course.semester');
+			return query.find();
+		}).then(successCallback, parseErrorHandler);
 	},
 	//Gets ALL of the events that the current user can see
-	getAll: function(displayEvents) {
-		db.transaction(function (transaction) {
-			var sqlString = "SELECT *, event.id AS event_id, event.name AS name, upvotes.count AS upvotes, downvotes.count AS downvotes FROM event "
-				+ "JOIN user_course ON event.course_id = user_course.course_id "
-				+ "JOIN course ON event.course_id = course.id "
-				+ "LEFT OUTER JOIN (SELECT COUNT(value) AS count, vote.event_id AS event_id2 FROM vote WHERE value > 0 GROUP BY event_id2) AS upvotes ON upvotes.event_id2 = event_id "
-				+ "LEFT OUTER JOIN (SELECT COUNT(value) AS count, vote.event_id AS event_id2 FROM vote WHERE value < 0 GROUP BY event_id2) AS downvotes ON downvotes.event_id2 = event_id "
-				+ "WHERE user_course.user_id = ? "
-			    + "ORDER BY due_date ASC";
+	getAll: function(successCallback) {
+		var userCourseQuery = new Parse.Query(CourseObject);
+		userCourseQuery.equalTo('members', Parse.User.current());
 
-			transaction.executeSql(sqlString, [User.getCurrent().id],
-				displayEvents, errorHandler);
-		});
+		var query = new Parse.Query(EventObject);
+		query.include('course').matchesQuery('course', userCourseQuery).ascending('dueDate').find().then(successCallback, parseErrorHandler);
 	},
-	//Drop and re-init the table
-	nuke: function() {
-		db.transaction(function(transaction) {
-			transaction.executeSql("DROP TABLE IF EXISTS event", [], Event.initialize, errorHandler);
-		}, errorHandler);
+	vote: function(event_id, value, successCallback) {
+		var hadUpvoted = false;
+		var hadDownvoted = false;
+
+		Event.read(event_id, function(event) {
+			var ups = event.relation('upvoters').query();
+			ups.equalTo("objectId", Parse.User.current().id);
+
+			var downs = event.relation('downvoters').query();
+			downs.equalTo("objectId", Parse.User.current().id);
+
+			ups.count().then(function(upCount) {
+				hadUpvoted = upCount > 0;
+				return downs.count();
+			}).then(function(downCount) {
+				hadDownvoted = downCount > 0;
+
+				if (hadUpvoted || hadDownvoted) {
+					if (!hadDownvoted && hadUpvoted && value < 0) {
+						//Remove upvote, add downvote
+						event.increment('upvotes', -1);
+						event.increment('downvotes');
+						event.relation('upvoters').remove(Parse.User.current());
+						event.relation('downvoters').add(Parse.User.current());
+						event.save();
+					} else if (!hadUpvoted && hadDownvoted && value > 0) {
+						//Remove downvote, add upvote
+						event.increment('upvotes');
+						event.increment('downvotes', -1);
+						event.relation('upvoters').add(Parse.User.current());
+						event.relation('downvoters').remove(Parse.User.current());
+						event.save();
+					}
+				} else {
+					if (value > 0) {
+						//Add new upvote
+						event.increment('upvotes');
+						event.relation('upvoters').add(Parse.User.current());
+						event.save();
+					} else if (value < 0) {
+						//Add new downvote
+						event.increment('downvotes');
+						event.relation('downvoters').add(Parse.User.current());
+						event.save();
+					}
+				}
+			});
+		});
 	}
-}
+};
