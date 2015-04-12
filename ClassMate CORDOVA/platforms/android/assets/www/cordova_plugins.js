21932ae3fbebb12c019ab8a56e4d94255768a2a4
Kyle Zimmerman <zimmerkyle@gmail.com>
2015-04-12 15:46:37 -0400

    Added Push Notifications

--- ClassMate CORDOVA/platforms/android/assets/www/cordova_plugins.js
+++ ClassMate CORDOVA/platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.ParsePushPlugin/www/parse-push-plugin.js",
+        "id": "com.phonegap.plugins.ParsePushPlugin.ParsePushPlugin",
+        "clobbers": [
+            "ParsePushPlugin"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.phonegap.plugins.ParsePushPlugin": "0.3.0"
+}
 // BOTTOM OF METADATA
 });
